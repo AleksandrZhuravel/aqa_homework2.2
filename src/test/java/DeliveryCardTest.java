c5c3445cda705f3d2803c11b109901c1de598658
AleksandrZhuravel <cah40c@tut.by>
2019-12-18 13:42:42 +0300

    Test and README changed from modified in according to the comments of Maksim Batirev

--- src/test/java/DeliveryCardTest.java
+++ src/test/java/DeliveryCardTest.java
@@ -2,6 +2,7 @@ import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -10,8 +11,8 @@ import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class DeliveryCardTest {
-    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-    String date = LocalDate.now().plusDays(4).format(formatter);
+    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private String date = LocalDate.now().plusDays(4).format(formatter);
 
     @Test
     @DisplayName("Should show confirmation message if all fields completed correctly")
@@ -19,7 +20,7 @@ public class DeliveryCardTest {
         open("http://localhost:9999");
         SelenideElement form = $(By.className("App_appContainer__3jRx1"));
         form.$("[data-test-id = city] input").setValue("Челябинск");
-        form.$("[data-test-id = date] input").sendKeys("\b\b\b\b\b\b\b\b\b\b");
+        form.$("[data-test-id = date] input").sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.DELETE));
         form.$("[data-test-id = date] input").setValue(date);
         form.$("[data-test-id = name] input").setValue("Иванов Иван");
         form.$("[data-test-id = phone] input").setValue("+79270000000");
